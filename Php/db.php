80d117bb40a1673a81cd5a767e68876391c9ba49
AurimasKaseta <72064844+AurimasKaseta@users.noreply.github.com>
2021-03-17 09:19:51 +0200

    Update db.php

--- Php/db.php
+++ Php/db.php
@@ -7,9 +7,9 @@ function Createdb()
     $dbname="bookstore";
 
 // Create connection
-    $con = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password);
 // Check connection
-    if ($con->connect_error) {
+    if ($conn->connect_error) {
         die("Connection failed: " . $con->connect_error);
     }
 
